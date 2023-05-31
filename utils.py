04e8a50a0a99b8264859106fb56b68415551afde
Ozowee <rciach@outlook.com>
2023-05-31 12:06:20 +0200

    added tls_client

--- utils.py
+++ utils.py
@@ -1,25 +1,23 @@
 import requests
+import tls_client
 import matplotlib.pyplot as plt
 import numpy
 from bs4 import BeautifulSoup
 from pprint import pprint
+from random_user_agent.params import SoftwareName, HardwareType
+from random_user_agent.user_agent import UserAgent
+
+software_names = [SoftwareName.CHROME.value]
+hardware_type = [HardwareType.MOBILE__PHONE]
+user_agent_rotator = UserAgent(software_names=software_names, hardware_type=hardware_type)
 
 class GetProducts(): 
     def __init__(self,query):
+        self.session = tls_client.Session(     client_identifier="chrome_113" )
         self.headers = {
             'Host': 'www.ceneo.pl',
-            'Cache-Control': 'max-age=0',
-            'sec-ch-ua': '"Chromium";v="112", "Not_A Brand";v="24", "Opera GX";v="98"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 OPR/98.0.0.0',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 OPR/98.0.0.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'Sec-Fetch-Site': 'same-origin',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-User': '?1',
-            'Sec-Fetch-Dest': 'document',
-            'Referer': 'https://www.ceneo.pl/',
             'Accept-Language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7',
         }
         self.query = query
@@ -28,8 +26,8 @@ class GetProducts():
     def ScrapProducts(self):
 
         keyword = self.query.replace(" ","+")
-        req_scrap = requests.get(f'https://www.ceneo.pl/Smartfony;szukaj-{keyword}', headers=self.headers)
-
+        req_scrap = self.session.get(f'https://www.ceneo.pl/Smartfony;szukaj-{keyword}', headers=self.headers)
+        print(req_scrap.text)
         if req_scrap.status_code != 200:
             print(f"Connection error, status code: {req_scrap.status_code}")
         else:
@@ -56,21 +54,21 @@ class GetProducts():
                             productImageUrl = "https:"+str(containerImage).split('href="')[1].split('"')[0]
                 except IndexError:
                     productScore = "N/A"
-                try:
-                    self.AllProductsDetails[productName] = {
-                        "productID":productID,
-                        "productPrice":productPrice,
-                        "productIMG":productImageUrl,
-                        "productScore":productScore,
-                        "productReviews":productReviews
+                #try:
+                self.AllProductsDetails[productName] = {
+                    "productID":productID,
+                    "productPrice":productPrice,
+                    "productIMG":productImageUrl,
+                    "productScore":productScore,
+                    "productReviews":productReviews
                                                             }
-                except UnboundLocalError:
-                    self.AllProductsDetails["errorCode"] = {
-                        "ErrorInfo":"Wrong variable on input"
-                    }
+                # except UnboundLocalError:
+                #     self.AllProductsDetails["errorCode"] = {
+                #         "ErrorInfo":"Wrong variable on input"
+                #     }
             print(self.AllProductsDetails)
     def GetSpecificProduct(self,productID):
-        req_specific = requests.get(f'https://www.ceneo.pl/{productID}',headers=self.headers)
+        req_specific = self.session.get(f'https://www.ceneo.pl/{productID}',headers=self.headers)
         if req_specific.status_code !=200:
             print(f"Connection error, status code: {req_specific.status_code}")
         else:
@@ -122,8 +120,8 @@ class GetProducts():
         print("Graph successfully generated!")
 
         
-iphone = GetProducts("iphone 14 pro")
+#iphone = GetProducts("iphone 14 pro")
 #iphone.ScrapProducts()
-iphone.GetSpecificProduct("138536499")
-iphone.PriceGraph()
+#iphone.GetSpecificProduct("138536499")
+#iphone.PriceGraph()
 
